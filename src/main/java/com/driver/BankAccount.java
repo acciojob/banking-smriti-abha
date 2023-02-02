0201e93217474ad63f3ecd8b48b3f38ddc85d100
Abha Kumari <smritiabha201099@gmail.com>
2023-02-03 00:46:47 +0530

    Done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,70 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+       if(sum>9*digits){
+           throw new Exception("Account Number can not be generated");
+       }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digits; i++) {
+            if (sum >= 9) {
+                sb.append('9');
+                sum -= 9;
+            } else {
+                sb.append((char) ('0' + sum));
+                sum = 0;
+            }
+        }
 
-        return null;
+        return sb.toString();
     }
 
     public void deposit(double amount) {
         //add amount to balance
+       amount =amount+balance;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+           if(this.balance-amount>=getMinBalance()){
+               amount= balance-amount;
+           }else{
+               throw new Exception("Insufficient Balance");
+           }
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,6 +5,12 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+
+        this.tradeLicenseId=tradeLicenseId;
+        if(balance<5000){
+            throw new Exception("Insufficient Balance");
+        }
 
     }
 
@@ -13,7 +19,83 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if (!isNumberValid(tradeLicenseId)) {
+            String rearrangedId = arrangeString(tradeLicenseId);
+            if (rearrangedId == "") {
+                throw new Exception("Valid License can not be generated");
+            } else {
+                this.tradeLicenseId = rearrangedId;
+            }
+        }
 
+    }
+    public char getCountChar(int[] count) {
+        int max = 0;
+        char ch = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                ch = (char) ((int) 'A' + i);
+            }
+        }
+
+            return ch;
+        }
+
+    public String arrangeString(String S) {
+        int N = S.length();
+
+        int[] count = new int[26];
+        for (int i = 0; i < 26; i++) {
+            count[i] = 0;
+        }
+        for (char ch : S.toCharArray()) {
+            count[(int) ch - (int) 'A']++;
+        }
+
+        char ch_max = getCountChar(count);
+        int maxCount = count[(int) ch_max - (int) 'A'];
+
+        if (maxCount > (N + 1) / 2)
+            return "";
+
+        String res = "";
+        for (int i = 0; i < N; i++) {
+            res += ' ';
+        }
+
+        int ind = 0;
+        while (maxCount > 0) {
+            res = res.substring(0, ind) + ch_max
+                    + res.substring(ind + 1);
+            ind = ind + 2;
+            maxCount--;
+        }
+        count[(int) ch_max - (int) 'A'] = 0;
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 0) {
+                ind = (ind >= N) ? 1 : ind;
+                res = res.substring(0, ind)
+                        + (char) ((int) 'A' + i)
+                        + res.substring(ind + 1);
+                ind += 2;
+                count[i]--;
+            }
+        }
+        return res;
+    }
+    public boolean isNumberValid(String licenseId) {
+        for (int i = 0; i < licenseId.length() - 1; i++) {
+            if (licenseId.charAt(i) == licenseId.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
     }
 
 }
+
+
